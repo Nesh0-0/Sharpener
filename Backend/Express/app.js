9a08d35e421511ea7e74cb02d55a77a25aaec44d
Nesh0-0 <ganesan.s99@gmail.com>
2025-06-26 21:37:34 +0530

    Express middle update

--- Backend/Express/app.js
+++ Backend/Express/app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
+app.use("/welcome", (req, res, next) => {
+    console.log('Middleware executed');
+    req.user = 'Guest';
+    next();
+});
+
+app.get('/welcome', (req, res) => {
+    res.send(`<h1>Welcome, ${req.user}</h1>`);
+});
+
+
+
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
