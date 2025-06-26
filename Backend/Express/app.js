b24a4927d7682469a2d1158899f8deb33b1784ca
Nesh0-0 <ganesan.s99@gmail.com>
2025-06-26 23:17:38 +0530

    added middleware for products and categories

--- Backend/Express/app.js
+++ Backend/Express/app.js
@@ -2,12 +2,33 @@ const express = require('express');
 const app = express();
 
 
+app.use((req, res, next) => {
+    const method = req.method;
+    const url = req.url;
+    console.log(`Request Method: ${method}, Request URL: ${url}`);
+    next();
+});
 
-app.get('/welcome/:username', (req, res) => {
-    const username = req.params.username;
-    const {role} = req.query;
-    res.send(`Welcome ${username}, your role is ${role}`)
-})
+
+app.get('/products', (req, res) => {
+    res.send('Here is a list of all products.');
+});
+
+app.post('/products', (req, res) => {
+    res.send('A new product has been created.');
+});
+
+app.get('/categories', (req, res) => {
+    res.send('Here is a list of all categories.');
+});
+
+app.post('/categories', (req, res) => {
+    res.send('A new category has been added.');
+});
+
+app.use((req, res) => {
+  res.status(404).send('<h1>404 - Page Not Found</h1>');
+});
 
 
 
