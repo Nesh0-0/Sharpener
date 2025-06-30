19e048fb878ff44d807df1bcde9c111a1ea8706a
Nesh0-0 <ganesan.s99@gmail.com>
2025-06-30 17:33:29 +0530

    CRUD Operations using sequelize

--- Backend/SQL/controllers/studentsController.js
+++ Backend/SQL/controllers/studentsController.js
@@ -1,92 +1,110 @@
 const { get } = require('../routes/studentRoutes');
 const db = require('../utils/db');
+const student = require('../models/Students');
 
-const addStudent = (req, res) => {
-    const { name, email, age } = req.body;
-    const insertQuery = `INSERT INTO Students(name, email, age) VALUES('${name}', '${email}', ${age})`;
-    db.execute(insertQuery, (err) => {
-        if (err) {
-            console.error(`Error inserting student ${name}:`, err);
-            res.status(500).send(`Error inserting student ${name}`);
-            db.close();
-            return;
-        }
+const addStudent = async (req, res) => {
+    try {
+        const { name, email, age } = req.body;
+        const insert = await student.create( {
+            'name': name,
+            'email': email
+        });
         res.status(201).send('Student inserted successfully');
+    }
+    catch (err) {
+        console.log('Error inserting student! ' + err);
+        res.send(500).send('Error inserting student!');
+    }
 
+};
 
-    });
+const deleteStudent = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleteStudent = await student.destroy({
+            where: {
+                id: id
+            }
+        });
 
-}
-
-const deleteStudent = (req, res) => {
-    const { id } = req.params;
-    const deleteQuery = `DELETE FROM Students WHERE id = ${id}`;
-    db.execute(deleteQuery, (err) => {
-        if (err) {
-            console.log('Error deleting student:', err);
+        if (!deleteStudent) {
+            console.log('No such student exist!');
             res.status(500).send('No such student found!');
-            return;
+            return
         }
-        res.status(200).send('Student deleted successfully!');
-    });
+        res.status(200).send('Student details deleted successfully!');
+    }
+    catch (err) {
+        console.log('Error deleteing student details!' + err);
+        res.status(500).send('Error deleting student details!');
+    }
 };
 
 
-const updateStudent = (req, res) => {
-    const { id } = req.params;
-    const { name, email } = req.body;
-
-    if (!name) {
-        res.status(400).send('Name is required');
-        return; 
-    }
-    if (!email) {
-        res.status(400).send('Email is required');
-        return; 
-    }
-    const updateQuery = `UPDATE Students SET name = '${name}', email = '${email}' WHERE id = ${id}`;
-    db.execute(updateQuery,[name, email] ,(err, result) => {
-        if (err) {
-            console.log('Error updating student:', err);
-            res.status(500).send('Error updating student');
+const updateStudent = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const {name, email} = req.body;
+        console.log(id, name, email);
+        if (!name) {
+            res.status(400).send('Name is required');
+            return; 
+        }
+        if (!email) {
+            res.status(400).send('Email is required');
+            return; 
+        }
+        const studentDetails = await student.findByPk(id);
+        if (!student) {
+            console.log('No such student found!')
+            res.status(500).send('Student with the given id does not exist!');
             return;
         }
-        if (result.affectedRows === 0) {
-            res.status(404).send('No such student found!');
-            return;
-        }
-        // console.log('User updated successfully!');
-        res.status(200).send('Student name and email updated successfully!');
-    })
-
+        console.log("success");
+        studentDetails.save();
+        res.status(200).send('Student details updated successfully!');
+        
+    }
+    catch (err) {
+        console.log('Error updating student details!' + err);
+        res.status(500).send('Error updating student details!');
+    }
 }
 
 
 
-const getAllStudents = (req, res) => {
-    const selectQuery = `SELECT * FROM Students`;
-    db.execute(selectQuery, (err, result) => {
-        console.log('Result:', result);
-        res.status(200).json(result);
-    });
+const getAllStudents = async (req, res) => {
+    try {
+        const selectStudents = await student.findAll();
+        if (!selectStudents) {
+            res.status(500).send('No students found!');
+            return;
+        }
+        res.status(200).json(selectStudents);
+    }
+    catch (err) {
+        console.log('Error fetching students!' + err);
+        res.send(500).send('Error fetching students!');
+    }
 
 }
 
-const getStudentById = (req, res) => {
-    const {id} = req.params;
-    const selectQuery = `SELECT * FROM Students WHERE id = ${id}`;
-    db.execute(selectQuery, (err, result) => {
-        if (err) {
-            console.error(`Error fetching student with ID ${id}:`, err);
-            res.status(500).send(`Error fetching student with ID ${id}`);
+const getStudentById = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const studentDetails = await student.findByPk(id);
+
+        if (!studentDetails) {
+            res.status(500).send('Student with the given id does not exist!');
             return;
         }
-        if (result.length === 0) {
-            res.status(404).send('No such student found!');
-            return;
-        }
-        res.status(200).json(result);
-    });
+        res.status(200).json(studentDetails);
+
+    }
+    catch (err) {
+        console.log('Error finding student!' + err);
+        res.send(500).send('Error finding student!');
+    }
 };
 
 
